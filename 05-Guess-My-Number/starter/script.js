e9eb593f188c2a2e9975b67f4ccafd3f5f713f93
onisanya <onisanya@gmail.com>
2025-02-23 18:03:02 +0100

    Guess start

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Lets check!';
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
