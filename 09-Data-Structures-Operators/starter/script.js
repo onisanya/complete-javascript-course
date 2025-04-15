0f5743ad88d26736b9e89baab6ad12dbe1090dff
onisanya <onisanya@gmail.com>
2025-04-15 10:23:52 +0200

    JS course updates

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -45,3 +45,5 @@ const restaurant = {
     },
   },
 };
+
+console.log(restaurant?.hours?.thu.open);
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,54 @@
 'use strict';
+
+const poll = {
+  question: 'What is your favorite programming language?',
+  options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+};
+
+const displayPoll = function (str) {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('poll-container');
+  const newHeading = document.createElement('h2');
+  newHeading.textContent = str;
+  newDiv.appendChild(newHeading);
+  // document.querySelector('.container').appendChild(newDiv);
+  document
+    .querySelector('.container')
+    .insertAdjacentElement('afterend', newDiv);
+};
+
+const getListedOptions = function () {
+  let str = Object.values(poll.options);
+
+  //str.replaceAll(/:/g, '').trim();
+  // console.log(str);
+  console.log(str[0]);
+};
+getListedOptions();
+
+displayPoll('Javascript');
+/*
+const insertPoll = function () {
+  const options = poll.options.split;
+
+  const tmp = [...poll.options];
+
+  for (let i = 0; i < 4; ) {
+    const pollItem = prompt(
+      `What is the #${i + 1} in your poll?`,
+      tmp.join(', ')
+    );
+    i++;
+
+    pollItem = Number(pollItem);
+
+    console.log(typeof pollItem);
+    if (typeof pollItem === 'number' && !NaN(pollItem)) {
+      if (pollItem >= 0 && pollItem <= 3) {
+        console.log('Valid option!');
+      }
+    }
+  }
+};
+*/
