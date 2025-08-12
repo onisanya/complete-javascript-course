2fb162f874d74a98dc99659f004f85c26c1840ab
onisanya <onisanya@gmail.com>
2025-08-12 17:28:26 +0200

    Added popup on click functional

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -46,10 +46,23 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([latitude, longitude])
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              maxHeight: 100,
+              autoClose: false,
+              closeOnClick: false,
+            })
+          )
+
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Cannot get your position');
